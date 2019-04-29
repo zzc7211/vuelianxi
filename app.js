1c258bcc8799d09142a82eb2cc5619c2942ee0bb
zzc <914880597@qq.com>
2019-04-29 22:39:00 +0800

    动态CSS样式

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name:"测试元",
-        age: 25,
-        a:0,
-        b:0,
+        changeColor:false,
+        changeLength:false,
     },
     methods: {
-        addtoA:function () {
-            console.log("Add to A");
-            return this.a+this.age;
-        },
-        addtoB:function () {
-            console.log("Add to B");
-            return this.b+this.age;
-        }
+
     },
     computed:{
-        c_addtoA:function () {
-            console.log("computed 属性计算Add to A");
-            return this.a+this.age;
-        },
-        c_addtoB:function () {
-            console.log("computed 属性计算Add to B");
-            return this.b+this.age;
+
+        compClasses:function () {
+            return {
+                changeColor: this.changeColor,
+                changeLength: this.changeLength
+            }
+
         }
     }
 });
